99ec4eaeb933f5b35899f090b91ea420f8220c0c
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-03-02 15:17:05 +0500

    Speicals section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,9 +42,29 @@ window.addEventListener("scroll", scrollHeader);
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
+let mixerProducts = mixitup(".products__content", {
+  selectors: {
+    target: ".products__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
 /* Default filter products */
 
+mixerProducts.filter(".delicacies");
+
 /* Link active products */
+
+const linkProducts = document.querySelectorAll(".products__item");
+
+function activeProducts() {
+  linkProducts.forEach((l) => l.classList.remove("active-products"));
+  this.classList.add("active-products");
+}
+
+linkProducts.forEach((l) => l.addEventListener("click", activeProducts));
 
 /*=============== SHOW SCROLL UP ===============*/
 
